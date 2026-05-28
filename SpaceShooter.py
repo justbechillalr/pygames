8dbdb8b103bc38a38d795c709ce3974d058849d0
justbechillalr <abdullahihusseinali123gmail.com>
2026-05-28 23:39:05 +0200

    all nighter

--- SpaceShooter.py
+++ SpaceShooter.py
@@ -1,5 +1,6 @@
 #imports pygame
 import pygame
+import random
 #launches pygame
 pygame.init()
 Screen_width, Screen_height = ((800,600))
@@ -13,17 +14,15 @@ last_shot_time = 0
 normal_color= (0,255,0)
 hit_color = (255,0,0)
 
-
-
 #Clock settings#
 clock = pygame.time.Clock()
 #Box spawn#
-box_y = Screen_height / 1.2
-box_x = Screen_width / 2
+player_y = Screen_height / 1.2
+player_x = Screen_width / 2
 #Box settings#
-box_size = 50
-box_height = 50
-box_width =  50
+player_size = 50
+player_height = 50
+player_width =  50
 #bullets settings#
 bullet_width = 10
 bullet_height = 10
@@ -32,11 +31,15 @@ bullets = []
 #Game settings#
 speed = 10
 #text dumy spawn#
-dummy_y = Screen_height//2
-dummy_x = Screen_width //2
-dummy_plane = pygame.Rect(dummy_x,dummy_y,box_width,box_height)
-dummy_color = normal_color
+target_y = Screen_height//2
+target_x = Screen_width //2
+target = pygame.Rect(target_x,target_y,player_width,player_height)
+target_color = normal_color
+target_list = pygame.Rect(target_x, target_y, player_width, player_width)
 
+#statement
+
+#fly = (Screen_width//2, Screen_height//2, size, size)
 
 #dummy/bullet colide settings#
 #main game loop#
@@ -51,52 +54,59 @@ while running:
         #Movement Settings#
     keys = pygame.key.get_pressed()
     if keys[pygame.K_a]:
-        box_x -= speed
+        player_x -= speed
     if keys[pygame.K_d]:
-        box_x += speed
+        player_x += speed
+    if keys[pygame.K_w]:
+        player_y -= speed
+    if keys[pygame.K_s]:
+        player_y += speed
     if keys[pygame.K_SPACE]:
         if now - last_shot_time > Shoot_cooldown:
+            testBullet = pygame.Rect(target_x,target_y,player_width,player_height)
+            colliding = testBullet.colliderect(target)
             #Fire bullets
-            bullet_x = box_x + box_size //2
-            bullet_y = box_y + box_size // 2 - bullet_height // 2
+            bullet_x = player_x + player_size //2
+            bullet_y = player_y + player_size // 2 - bullet_height // 2
             bullets.append([bullet_x, bullet_y])
             #resets cooldown#
             last_shot_time = now
 
     #move the bullet#
-    for bullet in bullets[:]:
-        bullet[1] -= bullet_speed
-        testBullet = pygame.Rect(bullet[0],bullet[1],bullet_width,bullet_height)
-        colliding = testBullet.colliderect(dummy_plane)
+    
+        for bullet in bullets[:]:
+            bullet[1] -= bullet_speed
+            testBullet = pygame.Rect(bullet[0],bullet[1],bullet_width,bullet_height)
         
+    
         if colliding:
+            target_color = hit_color
             bullets.remove(bullet)
-            dummy_color = hit_color
             
 
-        if bullet[1] > Screen_height:
-            bullets.remove(bullet)
-
         
-   
+
+
+                                    
     #keeps Box in screen#
-    box_x = max (0, min(Screen_width- box_width,box_x))
-    box_y = max (0, min(Screen_height- box_height,box_y))
+    player_x = max (0, min(Screen_width  - player_width,player_x))
+    player_y = max (0, min(Screen_height - player_height,player_y))
     #background color#
     
-    #screeen#
+    #screeen#                                LOADING SETTINGS
     screen.fill((0,0,0))
-       #test box#
+
+       #Target#
     pygame.draw.rect(
         screen,
-        dummy_color,
-        (dummy_x,dummy_y,box_width,box_height)
+        normal_color,
+        (target_x,target_y,player_width,player_height)
     )
     #laster inn Box#
     pygame.draw.rect(
         screen,
-        (0,200,255),
-        (box_x,box_y,box_width,box_height)
+        (0,255,255),
+        (player_x,player_y,player_width,player_height)
     )
     #Loading bullets#
     for bullet in bullets[:]:
